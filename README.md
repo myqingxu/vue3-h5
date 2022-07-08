0de162937aff6fa9755e09207c3d897ac866b3ed
xuqiang <xuqiang@yuanxinjituan.com>
2022-07-08 11:06:32 +0800

    feat: 添加eslint配置和 stylelint的配置

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ This is a vue3 family bucket built h5, out of the box
 
 1. npm install
 2. npm run dev
+
+
+> eslint 和 stylelint 参考至掘金博客 https://juejin.cn/post/7022720509875847182
--- src/views/Tabbar.vue
+++ src/views/Tabbar.vue
@@ -1,22 +1,28 @@
 <template>
   <router-view />
   <van-tabbar route>
-    <van-tabbar-item replace to="/home" icon="home-o">首页</van-tabbar-item>
+    <van-tabbar-item replace to="/home" icon="home-o">首页 </van-tabbar-item>
     <van-tabbar-item replace to="/message" icon="search">消息</van-tabbar-item>
   </van-tabbar>
 </template>
 
 <script>
 import { Tabbar, TabbarItem } from 'vant'
-
+const isAbsent = Symbol()
 export default {
   name: 'HelloWorld',
   components: {
     Tabbar,
     TabbarItem
   },
-  data() {
-    return {}
+  props: {
+    foo: {
+      default: isAbsent
+    }
+  },
+  // 一个组件选项，在组件被创建之前，props 被解析之后执行。它是组合式 API 的入口。
+  setup(props) {
+    console.log('props:', props.foo === isAbsent) // -> true
   }
 }
 </script>
